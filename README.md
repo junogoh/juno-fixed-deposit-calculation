bdd6c40186ba4550d51802a6a2bbe91f5d97ff35
junogoh <junogoh@junos-MacBook-Pro.local>
2017-02-11 01:41:24 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+method 1 = regular deposits (零存整付)
+method 2 = Round-amount savings (整存整付)
+method 3 = withdrawals of interest savings (存本取息)
---
+++ method3.js
@@ -0,0 +1,20 @@
+var initial_amount = 120000;
+var interest_rate = 0.02;
+var accumulated_amount = initial_amount;
+
+var monthDisplay = ['Jan', 'Feb', 'March', 'April', 'May', 'June', 'July', 'August', 'Sept', 'Oct', 'Nov', 'Dec'];
+
+console.log("Month" + " " + "\t" + "Interest" + " " + "\t" + "Amount");
+console.log("=======================================================");
+for(var i = 0; i < 12; i++)
+{
+    var monthly_interest = roundTo2Decimals((initial_amount * interest_rate) / 12);
+ 
+    accumulated_amount = roundTo2Decimals((accumulated_amount + monthly_interest));
+
+    console.log(monthDisplay[i] + "\t" + monthly_interest + "\t" +  accumulated_amount);
+}
+
+function roundTo2Decimals(numberToRound) {
+  return Math.round(numberToRound * 100) / 100
+}
